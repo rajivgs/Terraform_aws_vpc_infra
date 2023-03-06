57225a0ae1f13bb58070e73f2ea05914d95ef1b7
[RajivGS] <[rajivgopalsingh@gmail.com]>
2023-03-06 22:14:39 +0545

    Route_Table_Integration

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 # Create a VPC 
 resource "aws_vpc" "main" {
   cidr_block       = var.aws_vpc_value
@@ -10,8 +9,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-
-# Create multiple public subnet for the VPC 
+# Create multiple public subnet for the VPC
 resource "aws_subnet" "public-subnet-01" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.aws_public_subnet-01
@@ -48,29 +46,129 @@ resource "aws_subnet" "private_subnet-02" {
   }
 }
 
-
 # Create the Internet Gateway for the VPC 
-
 resource "aws_internet_gateway" "vpc_igt" {
   vpc_id = aws_vpc.main.id
   tags = {
     Name = "IGT"
   }
-  
 }
 
-# Create the ElaticIP for the NGT
- resource "aws_eip" "elastic_ip" {
- vpc = true
- }
+# Create the Route Table for Internet Gateway
+#public route table
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc_igt.id
+  }
+  tags = {
+    Name = "Public-RT"
+  }
+}
+
+
+# Create the ElaticIP Address
+
+resource "aws_eip" "elastic_ip" {
+  vpc = true
+}
 
 
 # Create the NAT Gateway for the VPC
-
-resource "aws_nat_gateway" "vpc-ngt" {
+resource "aws_nat_gateway" "vpc_ngt" {
   allocation_id = aws_eip.elastic_ip.id
-  subnet_id = aws_subnet.public-subnet-01.id
+  subnet_id     = aws_subnet.public-subnet-01.id
   tags = {
-    "Name" = "NGT-VPC"
+    "Name" = "NGT"
   }
 }
+
+#private route table
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.vpc_ngt.id
+
+  }
+  tags = {
+    Name = "Private-RT"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+# # Creating VPC,name, CIDR and Tags
+# resource "aws_vpc" "dev" {
+#   cidr_block           = "10.0.0.0/16"
+#   instance_tenancy     = "default"
+
+#   tags = {
+#     Name = "dev"
+#   }
+# }
+
+# # Creating Public Subnets in VPC
+# resource "aws_subnet" "dev-public-1" {
+#   vpc_id                  = aws_vpc.dev.id
+#   cidr_block              = "10.0.1.0/24"
+#   map_public_ip_on_launch = "true"
+#   availability_zone       = "ap-south-1a"
+
+#   tags = {
+#     Name = "dev-public-1"
+#   }
+# }
+
+# resource "aws_subnet" "dev-public-2" {
+#   vpc_id                  = aws_vpc.dev.id
+#   cidr_block              = "10.0.2.0/24"
+#   map_public_ip_on_launch = "true"
+#   availability_zone       = "ap-south-1b"
+
+#   tags = {
+#     Name = "dev-public-2"
+#   }
+# }
+
+# # Creating Internet Gateway in AWS VPC
+# resource "aws_internet_gateway" "dev-gw" {
+#   vpc_id = aws_vpc.dev.id
+
+#   tags = {
+#     Name = "dev"
+#   }
+# }
+
+# # Creating Route Tables for Internet gateway
+# resource "aws_route_table" "dev-public" {
+#   vpc_id = aws_vpc.dev.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.dev-gw.id
+#   }
+
+#   tags = {
+#     Name = "dev-public-1"
+#   }
+# }
+
+# # Creating Route Associations public subnets
+# resource "aws_route_table_association" "dev-public-1-a" {
+#   subnet_id      = aws_subnet.dev-public-1.id
+#   route_table_id = aws_route_table.dev-public.id
+# }
+
+# resource "aws_route_table_association" "dev-public-2-a" {
+#   subnet_id      = aws_subnet.dev-public-2.id
+#   route_table_id = aws_route_table.dev-public.id
+# }
--- output.tf
+++ output.tf
@@ -1,34 +1,47 @@
-output "vpc_id" {    
-    description = "VPC ID: "
-  value = var.aws_vpc_value
+output "vpc_id" {
+  description = "VPC ID: "
+  value       = var.aws_vpc_value
 }
 
 output "private-subnet_id-01" {
-  description = "Subnet ID: "  
-  value = aws_subnet.private_subnet-01.id
+  description = "Subnet ID: "
+  value       = aws_subnet.private_subnet-01.id
 }
 output "private-subnet_id-02" {
-  description = "Subnet ID: "  
-  value = aws_subnet.private_subnet-02.id
+  description = "Subnet ID: "
+  value       = aws_subnet.private_subnet-02.id
 }
 output "public-subnet_id-01" {
-  description = "Subnet ID: "  
-  value = aws_subnet.public-subnet-01.id
+  description = "Subnet ID: "
+  value       = aws_subnet.public-subnet-01.id
 }
 output "public-subnet_id-02" {
-  description = "Subnet ID: "  
-  value = aws_subnet.public-subnet-02.id
+  description = "Subnet ID: "
+  value       = aws_subnet.public-subnet-02.id
 }
 
 output "internet_gateway_id" {
+  description = "Internet Gateway ID: "
   value = aws_internet_gateway.vpc_igt.id
 }
+
+output "public_route_table" {
+  description = "Public Route Table: "
+  value = aws_route_table.public-route-table
+}
+
+output "private_route_table" {
+    description = "Private Route Table: "
+  value = aws_route_table.private-route-table
+}
+
 output "nat_gateway_id" {
-  value = aws_nat_gateway.vpc-ngt.id
+    description = "NAT Gateway ID: "
+  value = aws_nat_gateway.vpc_ngt.id
 }
 
 output "elastic_ip" {
   description = "Elastic IP Address"
-  value = aws_eip.elastic_ip.id
+  value       = aws_eip.elastic_ip.id
 }
   
